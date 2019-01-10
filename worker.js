8255a40d23abcd3b58cf9c1bb771a81670dbd18d
nadav <tashernadav@gmail.com>
2019-01-10 23:17:25 +0200

    New Worker To Allow Offline Activity Now Scores Mostly 100s In Lighthouse

--- worker.js
+++ worker.js
@@ -1,2 +1,15 @@
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cacheResponse) {
+            return cacheResponse || fetch(event.request).then(function (fetchResponse) {
+                let responseClone = fetchResponse.clone();
+                caches.open('v1').then(function (cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return fetchResponse;
+            });
+        }).catch(function () {
+            return new Response("Offline");
+        })
+    );
 });
