e787ee6ea1b8aa0f28a07c09395cd86872e432df
nadav <tashernadav@gmail.com>
2019-01-11 16:41:11 +0200

    New SW, Offline Page

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # WebAppBase
 Base project for a mobile/desktop progressive web app.
+
+### Creating your own
+#### Download the latest version
+Git-Clone the repository or download a Release, and unpack it.
+
+#### Replace info
+Find `AppName` and replace it with your app name.
+
+Find `AppDescription` and replace it with your app description.
+
+Find `#FFFFFF` and replace it with your app color.
--- worker.js
+++ worker.js
@@ -1,9 +1,18 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('v1').then(function (cache) {
+            fetch("public/offline.html").then((response) => {
+                cache.put("offline.html", response);
+            });
+        })
+    );
+});
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         fetch(event.request).then(function (fetchResponse) {
             return fetchResponse;
         }).catch(function () {
-            return new Response("Offline");
+            return caches.match(new Request("offline.html")) || new Response("Offline");
         })
     );
 });
