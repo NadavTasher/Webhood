053fa4cfc3f8b61935b10af420551b6ef429ad4b
Nadav Tasger <tashernadav@gmail.com>
2019-10-05 18:53:48 +0300

    API PHP Documentation

--- scripts/backend/base/api.php
+++ scripts/backend/base/api.php
@@ -7,6 +7,13 @@
 
 $result = new stdClass();
 
+/**
+ * This function handles API calls by handing them over to the callback.
+ * @param string $api The API to listen to
+ * @param callable $callback The callback to be called with action and parameters
+ * @param bool $filter Whether to filter XSS characters
+ * @return mixed|null A result array with [success, result|error]
+ */
 function api($api, $callback, $filter = true)
 {
     if (isset($_POST[$api])) {
@@ -40,6 +47,11 @@ function api($api, $callback, $filter = true)
     return null;
 }
 
+/**
+ * This function filters XSS characters.
+ * @param string $source The original string
+ * @return string The filtered string
+ */
 function filter($source)
 {
     $source = str_replace("<", "", $source);
@@ -47,6 +59,13 @@ function filter($source)
     return $source;
 }
 
+/**
+ * This function writes to the $result object.
+ * @param string $api The API to write to
+ * @param string $type The data type to write
+ * @param string $key The data key
+ * @param string $value The data value
+ */
 function put($api, $type, $key, $value)
 {
     global $result;
@@ -55,6 +74,11 @@ function put($api, $type, $key, $value)
     $result->$api->$type->$key = $value;
 }
 
+/**
+ * This function generates a random string by the given length, with a complexity of 36^$length.
+ * @param int $length Length of random
+ * @return string Random string
+ */
 function random($length)
 {
     $current = str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz")[0];
@@ -64,11 +88,24 @@ function random($length)
     return "";
 }
 
+/**
+ * This function reports the result of the action.
+ * @param string $api The API to write to
+ * @param string $action The API call's action
+ * @param mixed|null $result The API call's result
+ */
 function result($api, $action, $result)
 {
     put($api, "result", $action, $result);
 }
 
+/**
+ * This function reports the success of the action.
+ * @param string $api The API to write to
+ * @param string $action The API call's action
+ * @param bool $success Whether the API call was successful
+ * @param string $error The API call's error
+ */
 function success($api, $action, $success = true, $error = "Unknown Error")
 {
     if ($success) {
