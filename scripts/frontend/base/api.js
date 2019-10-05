6b84d6d68d059af818a4a191cdce5193c3243b5f
Nadav Tasger <tashernadav@gmail.com>
2019-10-05 18:44:07 +0300

    API JS Documentation

--- scripts/frontend/base/api.js
+++ scripts/frontend/base/api.js
@@ -5,6 +5,15 @@
 
 /* API */
 
+/**
+ * This function is responsible for API calls between the frontend and the backend.
+ * @param endpoint The backend PHP file to be reached
+ * @param api The API which this call associates with
+ * @param action The action to be executed
+ * @param parameters The parameters for the action
+ * @param callback The callback for the API call, contains success, result and error
+ * @param form The form body to be sent along with this request (for API layering)
+ */
 function api(endpoint = null, api = null, action = null, parameters = null, callback = null, form = body()) {
     fetch(endpoint, {
         method: "post",
@@ -35,6 +44,14 @@ function api(endpoint = null, api = null, action = null, parameters = null, call
     });
 }
 
+/**
+ * This function compiles the API call body.
+ * @param api The API to associate
+ * @param action The action to be executed
+ * @param parameters The parameters for the action
+ * @param form The form body to be sent along with this request (for API layering)
+ * @returns {FormData} API call body
+ */
 function body(api = null, action = null, parameters = null, form = new FormData()) {
     if (api !== null && action !== null && parameters !== null && !form.has(api)) {
         form.append(api, JSON.stringify({
@@ -45,6 +62,10 @@ function body(api = null, action = null, parameters = null, form = new FormData(
     return form;
 }
 
+/**
+ * This function pops up installation instructions for Safari users.
+ * @param title App's Name
+ */
 function instruct(title = null) {
     let agent = window.navigator.userAgent.toLowerCase();
     let devices = ["iphone", "ipad", "ipod"];
@@ -76,6 +97,10 @@ function instruct(title = null) {
     }
 }
 
+/**
+ * This function prepares the web page (loads ServiceWorker, HTML).
+ * @param callback Function to be executed when loading finishes
+ */
 function prepare(callback = null) {
     // Register worker
     if ("serviceWorker" in navigator)
@@ -100,6 +125,10 @@ function prepare(callback = null) {
 
 /* Visuals */
 
+/**
+ * This function removes all children of given view.
+ * @param v View
+ */
 function clear(v) {
     let view = get(v);
     while (view.firstChild) {
@@ -107,18 +136,39 @@ function clear(v) {
     }
 }
 
+/**
+ * This function checks if a view exists.
+ * @param v View
+ * @returns {boolean} View exists
+ */
 function exists(v) {
     return get(v) !== undefined;
 }
 
+/**
+ * This function returns the view by its ID or by it's own value.
+ * @param v View
+ * @returns {HTMLElement} View
+ */
 function get(v) {
     return isString(v) ? document.getElementById(v) : v;
 }
 
+/**
+ * This function hides the given view.
+ * @param v View
+ */
 function hide(v) {
     get(v).style.display = "none";
 }
 
+/**
+ * This function creates a new view by its type, contents and classes.
+ * @param type View type
+ * @param content View contents
+ * @param classes View classes
+ * @returns {HTMLElement} View
+ */
 function make(type, content = null, classes = []) {
     let made = document.createElement(type);
     if (content !== null) {
@@ -139,6 +189,10 @@ function make(type, content = null, classes = []) {
     return made;
 }
 
+/**
+ * This function recursively sets the viewed view to be the given view.
+ * @param target View
+ */
 function page(target) {
     let temporary = get(target);
     while (temporary.parentNode !== document.body && temporary.parentNode !== document.body) {
@@ -148,10 +202,18 @@ function page(target) {
     view(temporary);
 }
 
+/**
+ * This function shows the given view.
+ * @param v View
+ */
 function show(v) {
     get(v).style.removeProperty("display");
 }
 
+/**
+ * This function shows the given view while hiding it's brothers.
+ * @param v View
+ */
 function view(v) {
     let element = get(v);
     let parent = element.parentNode;
@@ -161,6 +223,11 @@ function view(v) {
     show(element);
 }
 
+/**
+ * This function returns the visibillity state of the given view.
+ * @param v View
+ * @returns {boolean} Visible
+ */
 function visible(v) {
     return (get(v).style.getPropertyValue("display") !== "none");
 }
@@ -172,6 +239,14 @@ const RIGHT = !LEFT;
 const IN = true;
 const OUT = !IN;
 
+/**
+ * This function animates the given view's property, while jumping from stop to stop every length amount of time.
+ * @param v View
+ * @param property View's style property to animate
+ * @param stops Value stops
+ * @param length Length of each animation stop
+ * @param callback Function to run after animation is finished
+ */
 function animate(v, property = "left", stops = ["0px", "0px"], length = 1, callback = null) {
     let view = get(v);
     let interval = null;
@@ -198,6 +273,14 @@ function animate(v, property = "left", stops = ["0px", "0px"], length = 1, callb
     }, 0);
 }
 
+/**
+ * This function slides the given view in or out of frame with given direction.
+ * @param v View
+ * @param motion Type of motion (In / Out)
+ * @param direction Direction of motion (Left / Right)
+ * @param length Length of animation
+ * @param callback Function to run after animation is finished
+ */
 function slide(v, motion = IN, direction = RIGHT, length = 0.2, callback = null) {
     let view = get(v);
     let style = getComputedStyle(view);
@@ -213,26 +296,53 @@ function slide(v, motion = IN, direction = RIGHT, length = 0.2, callback = null)
 
 /* Attributes */
 
+/**
+ * This function makes the given view a column.
+ * @param v View
+ */
 function column(v) {
     get(v).setAttribute("column", true);
     get(v).setAttribute("row", false);
 }
 
+/**
+ * This function makes the given view an input.
+ * @param v View
+ */
 function input(v) {
     get(v).setAttribute("input", true);
 }
 
+/**
+ * This function makes the given view a row.
+ * @param v View
+ */
 function row(v) {
     get(v).setAttribute("row", true);
     get(v).setAttribute("column", false);
 }
 
+/**
+ * This function makes the given view a text.
+ * @param v View
+ */
 function text(v) {
     get(v).setAttribute("text", true);
 }
 
 /* Interface */
 
+/**
+ * This function sets the functions to be called when the user is performing gestures.
+ * @param up Function to run after an Up gesture
+ * @param down Function to run after a Down gesture
+ * @param left Function to run after a Left gesture
+ * @param right Function to run after a Right gesture
+ * @param upgoing Function to run while an Up gesture is gestured
+ * @param downgoing Function to run while a Down gesture is gestured
+ * @param leftgoing Function to run while a Left gesture is gestured
+ * @param rightgoing Function to run while a Rigjt gesture is gestured
+ */
 function gestures(up = null, down = null, left = null, right = null, upgoing = null, downgoing = null, leftgoing = null, rightgoing = null) {
     let touchX, touchY, deltaX, deltaY;
     document.ontouchstart = (event) => {
@@ -276,6 +386,14 @@ function gestures(up = null, down = null, left = null, right = null, upgoing = n
     };
 }
 
+/**
+ * This function pops up a popup at the bottom of the screen.
+ * @param contents The content to be displayed (View / Text)
+ * @param timeout The time before the popup dismisses (0 - Forever, null - Default)
+ * @param color The background color of the popup
+ * @param onclick The click action for the popup (null - Dismiss)
+ * @returns {function} Dismiss callback
+ */
 function popup(contents, timeout = null, color = null, onclick = null) {
     let div = make("div");
     column(div);
@@ -304,10 +422,10 @@ function popup(contents, timeout = null, color = null, onclick = null) {
         div.appendChild(contents);
     }
     animate(div, "opacity", ["0", "1"], 0.5, () => {
-        if (timeout !== null && timeout > 0) {
+        if (timeout > 0 || timeout === null) {
             setTimeout(() => {
                 dismiss();
-            }, timeout);
+            }, (timeout === null ? 2 : timeout) * 1000);
         }
     });
     document.body.appendChild(div);
@@ -316,18 +434,38 @@ function popup(contents, timeout = null, color = null, onclick = null) {
 
 /* Utils */
 
+/**
+ * This function returns whether the given parameter is an array.
+ * @param a Parameter
+ * @returns {boolean} Is an array
+ */
 function isArray(a) {
     return a instanceof Array;
 }
 
+/**
+ * This function returns whether the given parameter is an object.
+ * @param o Parameter
+ * @returns {boolean} Is an object
+ */
 function isObject(o) {
     return o instanceof Object && !isArray(o);
 }
 
+/**
+ * This function returns whether the given parameter is a string.
+ * @param s Parameter
+ * @returns {boolean} Is a string
+ */
 function isString(s) {
     return (typeof "" === typeof s || typeof '' === typeof s);
 }
 
+/**
+ * This function lets the user pick a file, then calls callback with the result.
+ * @param callback Function to run after file pick
+ * @param read Whether to read the file
+ */
 function upload(callback = null, read = false) {
     let selector = make("input");
     selector.type = "file";
@@ -355,6 +493,13 @@ function upload(callback = null, read = false) {
     selector.click();
 }
 
+/**
+ * This function downloads a file.
+ * @param file File's name
+ * @param data File's contents
+ * @param type File's mime-type
+ * @param encoding File's encoding
+ */
 function download(file, data, type = "text/plain", encoding = "utf8") {
     let link = document.createElement("a");
     link.download = file;
