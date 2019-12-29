44bd4305f88d49dced4286c08b9e1e312ba5c8fd
Nadav Tasger <tashernadav@gmail.com>
2019-12-29 16:28:01 +0200

    Added is_string and is_object check for better security

--- scripts/backend/base/api.php
+++ scripts/backend/base/api.php
@@ -23,20 +23,24 @@ function api($API, $callback, $filter = true)
             $request = filter($request);
         $APIs = json_decode($request);
         if (isset($APIs->$API)) {
-            if (isset($APIs->$API->action) && isset($APIs->$API->parameters)) {
-                $action = $APIs->$API->action;
-                $action_parameters = $APIs->$API->parameters;
-                $action_result = $callback($action, $action_parameters);
-                if (is_array($action_result)) {
-                    if (count($action_result) >= 2) {
-                        if (is_bool($action_result[0])) {
-                            $result->$API = new stdClass();
-                            $result->$API->success = $action_result[0];
-                            $result->$API->result = $action_result[1];
-                            if (count($action_result) >= 3) {
-                                return $action_result[2];
-                            } else {
-                                return $action_result;
+            if (isset($APIs->$API->action) &&
+                isset($APIs->$API->parameters)) {
+                if (is_string($APIs->$API->action) &&
+                    is_object($APIs->$API->parameters)) {
+                    $action = $APIs->$API->action;
+                    $action_parameters = $APIs->$API->parameters;
+                    $action_result = $callback($action, $action_parameters);
+                    if (is_array($action_result)) {
+                        if (count($action_result) >= 2) {
+                            if (is_bool($action_result[0])) {
+                                $result->$API = new stdClass();
+                                $result->$API->success = $action_result[0];
+                                $result->$API->result = $action_result[1];
+                                if (count($action_result) >= 3) {
+                                    return $action_result[2];
+                                } else {
+                                    return $action_result;
+                                }
                             }
                         }
                     }
