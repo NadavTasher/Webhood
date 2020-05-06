61e6dcc3a3a27698ec67715dee5ad92a026525db
nadav <tashernadav@gmail.com>
2020-05-06 22:48:59 +0300

    Renewed API

--- src/scripts/base/api.js
+++ src/scripts/base/api.js
@@ -31,35 +31,43 @@ class API {
      * @param endpoint API to call
      * @param action Action
      * @param parameters Parameters
-     * @param callback Callback
+     * @return Promise
      */
-    static call(endpoint = null, action = null, parameters = null, callback = null) {
-        // Create a form
-        let form = new FormData();
-        // Append parameters to form
-        for (let key in parameters) {
-            if (parameters.hasOwnProperty(key))
-                form.append(key, parameters[key]);
-        }
-        // Perform the request
-        fetch("apis/" + endpoint + "/" + "?" + action, {
-            method: "post",
-            body: form
-        }).then(response => response.text().then((result) => {
-            // Try to parse the result as JSON
-            try {
-                let API = JSON.parse(result);
-                // Check the result's integrity
-                if (API.hasOwnProperty("status") && API.hasOwnProperty("result")) {
-                    // Call the callback with the result
-                    callback(API["status"] === true, API["result"]);
-                } else {
-                    // Call the callback with an error
-                    callback(false, "API response malformed");
-                }
-            } catch (ignored) {
+    static call(endpoint = null, action = null, parameters = null) {
+        return new Promise((resolve, reject) => {
+            // Create a form
+            let form = new FormData();
+            // Append parameters to form
+            for (let key in parameters) {
+                if (parameters.hasOwnProperty(key))
+                    form.append(key, parameters[key]);
             }
-        }));
+            // Perform the request
+            fetch("apis/" + endpoint + "/" + "?" + action, {
+                method: "post",
+                body: form
+            }).then(response => response.text().then((result) => {
+                // Try to parse the result as JSON
+                try {
+                    let API = JSON.parse(result);
+                    // Check the result's integrity
+                    if (API.hasOwnProperty("status") && API.hasOwnProperty("result")) {
+                        // Call the callback with the result
+                        if (API["status"] === true) {
+                            resolve(API["result"]);
+                        } else {
+                            reject(API["result"]);
+                        }
+                    } else {
+                        // Call the callback with an error
+                        reject("API response malformed");
+                    }
+                } catch (ignored) {
+                    // Call the callback with an error
+                    reject("API response malformed");
+                }
+            }));
+        });
     }
 
 }
