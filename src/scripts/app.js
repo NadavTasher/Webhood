e796f064a146c97b6c8d3af4b06c975fb1e5b239
nadav <tashernadav@gmail.com>
2020-07-04 15:17:21 +0300

    JS update

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,3 @@
 function load() {
-    // App Load Code
-    Module.load("API", "UI", "Authenticate").then(() => {
-        Authenticate.initialize().then(() => {
-            console.log("Logged In");
-        });
-    });
+    // App Code
 }
-
-// App Code
