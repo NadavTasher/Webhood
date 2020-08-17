b36ab1b3e913164aece8c7bca0c7dc4a4f4032f9
nadav <tashernadav@gmail.com>
2020-08-17 17:34:23 +0300

    Base Node.JS backend work

---
+++ template/backend/routes/routes.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2020 Nadav Tasher
+ * https://github.com/NadavTasher/Template/
+ **/
+
+/**
+ * Exported routes.
+ */
+module.exports = {
+};
---
+++ template/backend/server.js
@@ -0,0 +1,20 @@
+// Import the HTTP module
+const HTTP = require("http");
+
+// Import the router module
+const route = require("./modules/route.js");
+
+// Import the file-server module
+const serve = require("./modules/serve.js");
+
+// Create a new HTTP server and start listening
+HTTP.createServer(function (request, response) {
+    // Check whether an API request needs to be processed
+    if (request.method === "POST") {
+        // Execute API logic
+        route(request, response);
+    } else {
+        // Serve static files
+        serve(request, response);
+    }
+}).listen(80);
