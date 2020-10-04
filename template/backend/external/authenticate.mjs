a5bea9bc2838bf80444a27dfefd15cf150ce361c
nadav <tashernadav@gmail.com>
2020-10-04 21:00:51 +0300

    New authentication API

--- template/backend/external/authenticate.mjs
+++ template/backend/external/authenticate.mjs
@@ -9,56 +9,17 @@ import { Table } from "../internal/database/database.mjs";
 import { Authority } from "../internal/utilities/token.mjs";
 import { Utilities } from "../internal/utilities/utilities.mjs";
 
-// Initialize the database table
-const mTable = new Table("users", {
-    information: {
-        name: "string"
-    },
-    authorization: {
-        password: {
-            hash: "hash",
-            salt: "id"
-        }
-    }
-});
-
 // Initialize a token issuer instance
 const mAuthority = new Authority(process.env.password);
 
-// Create the table of validators
-const mValidators = {
-    name: [
-        ("string"),
-        (name) => {
-            if (name.length < 4)
-                throw new Error(`Name too short`);
-
-            if (name.length > 16)
-                throw new Error(`Name too long`);
-
-            return true;
-        }
-    ],
-    password: [
-        ("string"),
-        (password) => {
-            if (password.length < 8)
-                throw new Error(`Password too short`);
-
-            if (password.length > 128)
-                throw new Error(`Password too long`);
-
-            return true;
-        }
-    ],
-    token: [
-        ("string"),
-        (token) => {
-            mAuthority.validate(token);
-            return true;
-        }
-    ],
-};
+// Initialize the database table
+const mTable = new Table("users", {
+    name: "string",
+    password: {
+        hmac: "hash",
+        secret: "id"
+    }
+});
 
 // Export user validator
 export function User(token) {
@@ -68,71 +29,77 @@ export function User(token) {
 
 // Export route
 export const Routes = {
+    issue: {
+        handler: (parameters) => {
+            // Generate a user ID
+            let id = Hash.hash(parameters.name);
+
+            // Check whether the user exists
+            if (mTable.has(id)) {
+                // Get user from table
+                let user = mTable.get(id);
+
+                // Make sure the password is correct
+                if (Hash.hmac(parameters.password, user.password.secret) !== user.password.hmac)
+                    throw new Error(`Password is incorrect`);
+            } else {
+                // Create secret and hmac
+                let secret = Utilities.random(64);
+                let hmac = Hash.hmac(parameters.password, secret);
+
+                // Set user in table
+                mTable.set(id, {
+                    name: parameters.name,
+                    password: {
+                        hmac: hmac,
+                        secret: secret
+                    }
+                });
+            }
+
+            // Issue a new token
+            return mAuthority.issue(id);
+        },
+        parameters: {
+            name: [
+                ("string"),
+                (name) => {
+                    if (name.length < 4)
+                        throw new Error(`Name too short`);
+
+                    if (name.length > 16)
+                        throw new Error(`Name too long`);
+
+                    return true;
+                }
+            ],
+            password: [
+                ("string"),
+                (password) => {
+                    if (password.length < 8)
+                        throw new Error(`Password too short`);
+
+                    if (password.length > 128)
+                        throw new Error(`Password too long`);
+
+                    return true;
+                }
+            ]
+        }
+    },
     validate: {
         handler: (parameters) => {
             // Return the name
             return mAuthority.validate(parameters.token);
         },
         parameters: {
-            token: mValidators.token
-        }
-    },
-    signUp: {
-        handler: (parameters) => {
-            // Generate a user ID
-            let id = Hash.hash(parameters.name);
-
-            // Make sure the user does not exist
-            if (mTable.has(id))
-                throw new Error(`User already exists`);
-
-            // Generate values
-            let salt = Utilities.random(32);
-            let hash = Hash.hash(parameters.password + salt);
-
-            // Write to the database
-            mTable.set(id, {
-                information: {
-                    name: parameters.name
-                },
-                authorization: {
-                    password: {
-                        hash: hash,
-                        salt: salt
-                    }
+            token: [
+                ("string"),
+                (token) => {
+                    mAuthority.validate(token);
+                    return true;
                 }
-            });
-
-            // Return token
-            return mAuthority.issue(id);
-        },
-        parameters: {
-            name: mValidators.name,
-            password: mValidators.password
-        }
-    },
-    signIn: {
-        handler: (parameters) => {
-            // Generate a user ID
-            let id = Hash.hash(parameters.name);
-
-            // Make sure the user exists
-            if (!mTable.has(id))
-                throw new Error(`User does not exist`);
-
-            // Read from the database
-            let object = mTable.get(id);
-
-            // Check password match
-            if (Hash.hash(parameters.password + object.authorization.password.salt) !== object.authorization.password.hash)
-                throw new Error(`Incorrect password`);
-
-            // Issue a token and return
-            return mAuthority.issue(id);
-        },
-        parameters: {
-            name: mValidators.name,
-            password: mValidators.password
+            ]
         }
     }
 };
--- template/frontend/modules/external/authenticate.js
+++ template/frontend/modules/external/authenticate.js
@@ -16,7 +16,7 @@ class Authenticate {
             let contents = document.body.innerHTML;
 
             // Load the new HTML
-            document.body.innerHTML = atob("PGRpdiByb3cgc3RyZXRjaD4KICAgIDxkaXYgc3R5bGU9Im1heC13aWR0aDogNTAwcHg7IiBjb2x1bW4+CiAgICAgICAgPCEtLSBUaXRsZXMgLS0+CiAgICAgICAgPHAgbGFyZ2UgY2VudGVyIGlkPSJ0aXRsZSI+V2VsY29tZSBiYWNrITwvcD4KICAgICAgICA8cCBzbWFsbCBjZW50ZXIgaWQ9Im91dHB1dCI+UGxlYXNlIGF1dGhlbnRpY2F0ZSB0byBjb250aW51ZTwvcD4KICAgICAgICA8IS0tIFVzZXIgaW50ZXJmYWNlcyAtLT4KICAgICAgICA8ZGl2IGlkPSJpbnRlcmZhY2UiIGhpZGRlbiBjb2x1bW4+CiAgICAgICAgICAgIDwhLS0gSW5wdXRzIC0tPgogICAgICAgICAgICA8ZGl2IGlkPSJpbnB1dHMiIGNvbHVtbj4KICAgICAgICAgICAgICAgIDxpbnB1dCBtZWRpdW0gbGVmdCBpZD0ibmFtZSIgdHlwZT0idGV4dCIgcGxhY2Vob2xkZXI9Ik5hbWUiIC8+CiAgICAgICAgICAgICAgICA8aW5wdXQgbWVkaXVtIGxlZnQgaWQ9InBhc3N3b3JkIiB0eXBlPSJwYXNzd29yZCIgcGxhY2Vob2xkZXI9IlBhc3N3b3JkIiAvPgogICAgICAgICAgICA8L2Rpdj4KICAgICAgICAgICAgPCEtLSBCdXR0b25zIC0tPgogICAgICAgICAgICA8ZGl2IGlkPSJidXR0b25zIiBjb2x1bW4+CiAgICAgICAgICAgICAgICA8ZGl2IHJvdyBzdHJldGNoPgogICAgICAgICAgICAgICAgICAgIDxidXR0b24gbWVkaXVtIGNlbnRlciBpZD0ic2lnblVwIj5TaWduIFVwPC9idXR0b24+CiAgICAgICAgICAgICAgICAgICAgPGJ1dHRvbiBtZWRpdW0gY2VudGVyIGlkPSJzaWduSW4iPlNpZ24gSW48L2J1dHRvbj4KICAgICAgICAgICAgICAgIDwvZGl2PgogICAgICAgICAgICAgICAgPGRpdiBoaWRkZW4gY29sdW1uPgogICAgICAgICAgICAgICAgICAgIDxidXR0b24gbWVkaXVtIGNlbnRlciBpZD0idmFsaWRhdGUiPlZhbGlkYXRlPC9idXR0b24+CiAgICAgICAgICAgICAgICA8L2Rpdj4KICAgICAgICAgICAgPC9kaXY+CiAgICAgICAgPC9kaXY+CiAgICA8L2Rpdj4KPC9kaXY+");
+            document.body.innerHTML = atob("PGRpdiByb3cgc3RyZXRjaD4KICAgIDxkaXYgc3R5bGU9Im1heC13aWR0aDogNTAwcHg7IiBjb2x1bW4+CiAgICAgICAgPCEtLSBUaXRsZXMgLS0+CiAgICAgICAgPHAgbGFyZ2UgY2VudGVyIGlkPSJ0aXRsZSI+V2VsY29tZSBiYWNrITwvcD4KICAgICAgICA8cCBzbWFsbCBjZW50ZXIgaWQ9Im91dHB1dCI+UGxlYXNlIGF1dGhlbnRpY2F0ZSB0byBjb250aW51ZTwvcD4KICAgICAgICA8IS0tIFVzZXIgaW50ZXJmYWNlcyAtLT4KICAgICAgICA8ZGl2IGlkPSJpbnRlcmZhY2UiIGhpZGRlbiBjb2x1bW4+CiAgICAgICAgICAgIDwhLS0gSW5wdXRzIC0tPgogICAgICAgICAgICA8ZGl2IGlkPSJpbnB1dHMiIGNvbHVtbj4KICAgICAgICAgICAgICAgIDxpbnB1dCBtZWRpdW0gbGVmdCBpZD0ibmFtZSIgdHlwZT0idGV4dCIgcGxhY2Vob2xkZXI9Ik5hbWUiIC8+CiAgICAgICAgICAgICAgICA8aW5wdXQgbWVkaXVtIGxlZnQgaWQ9InBhc3N3b3JkIiB0eXBlPSJwYXNzd29yZCIgcGxhY2Vob2xkZXI9IlBhc3N3b3JkIiAvPgogICAgICAgICAgICA8L2Rpdj4KICAgICAgICAgICAgPCEtLSBCdXR0b25zIC0tPgogICAgICAgICAgICA8ZGl2IGlkPSJidXR0b25zIiBjb2x1bW4+CiAgICAgICAgICAgICAgICA8YnV0dG9uIG1lZGl1bSByaWdodCBpZD0iY29udGludWUiPkNvbnRpbnVlPC9idXR0b24+CiAgICAgICAgICAgICAgICA8YnV0dG9uIG1lZGl1bSBjZW50ZXIgaWQ9InZhbGlkYXRlIiBoaWRkZW4+VmFsaWRhdGU8L2J1dHRvbj4KICAgICAgICAgICAgPC9kaXY+CiAgICAgICAgPC9kaXY+CiAgICA8L2Rpdj4KPC9kaXY+");
 
             // Store elements
             let titleElement = UI.find("title");
@@ -26,56 +26,35 @@ class Authenticate {
             titleElement.innerText = "Welcome to " + title;
 
             // Register event listeners
-            UI.find("signUp").addEventListener("click", (event) => {
+            UI.find("continue").addEventListener("click", (event) => {
                 // Hide the UI
                 UI.hide("interface");
 
                 // Display progress
-                outputElement.innerText = "Hold on - Signing up...";
+                outputElement.innerText = "Hold on - Issuing...";
                 outputElement.style.setProperty("color", null);
 
                 // Try to sign up
-                API.call("authenticate".toLowerCase(), "signUp", {
+                API.call("authenticate", "issue", {
                     name: UI.find("name").value,
                     password: UI.find("password").value
-                }).then((value) => {
-                    // Click the signIn button
-                    UI.find("signIn").click();
-                }).catch((reason) => {
-                    // Display reason
-                    outputElement.innerText = reason;
-                    outputElement.style.setProperty("color", "red");
+                }).then(
+                    (token) => {
+                        // Save the token
+                        localStorage.token = token;
 
-                    // Show the UI
-                    UI.show("interface");
-                });
-            });
+                        // Click the validation button
+                        UI.find("validate").click();
+                    }
+                ).catch(
+                    (reason) => {
+                        // Display reason
+                        outputElement.innerText = reason;
+                        outputElement.style.setProperty("color", "red");
 
-            UI.find("signIn").addEventListener("click", (event) => {
-                // Hide the UI
-                UI.hide("interface");
-
-                // Display progress
-                outputElement.innerText = "Hold on - Signing in...";
-                outputElement.style.setProperty("color", null);
-                // Try to sign in
-                API.call("authenticate".toLowerCase(), "signIn", {
-                    name: UI.find("name").value,
-                    password: UI.find("password").value
-                }).then((token) => {
-                    // Store the token
-                    localStorage.token = token;
-
-                    // Click the validate button
-                    UI.find("validate").click();
-                }).catch((reason) => {
-                    // Display reason
-                    outputElement.innerText = reason;
-                    outputElement.style.setProperty("color", "red");
-
-                    // Show the UI
-                    UI.show("interface");
-                });
+                        // Show the UI
+                        UI.show("interface");
+                    });
             });
 
             UI.find("validate").addEventListener("click", (event) => {
@@ -87,27 +66,31 @@ class Authenticate {
                 outputElement.style.setProperty("color", null);
 
                 // Try to validate
-                API.call("authenticate".toLowerCase(), "validate", {
+                API.call("authenticate", "validate", {
                     token: localStorage.token
-                }).then((userID) => {
-                    // Restore the HTML
-                    document.body.innerHTML = contents;
+                }).then(
+                    (userID) => {
+                        // Restore the HTML
+                        document.body.innerHTML = contents;
 
-                    // Resolve
-                    resolve(userID);
-                }).catch((reason) => {
-                    // Display reason
-                    outputElement.innerText = reason;
-                    outputElement.style.setProperty("color", "red");
+                        // Resolve
+                        resolve(userID);
+                    }
+                ).catch(
+                    (reason) => {
+                        // Display reason
+                        outputElement.innerText = reason;
+                        outputElement.style.setProperty("color", "red");
 
-                    // Show the UI
-                    UI.show("interface");
-                });
+                        // Show the UI
+                        UI.show("interface");
+                    }
+                );
             });
 
             // Automatic validation
-            if (localStorage.token !== null) {
-                // Click the validate button
+            if (localStorage.token) {
+                // Click the validation button
                 UI.find("validate").click();
             } else {
                 // Show the UI
