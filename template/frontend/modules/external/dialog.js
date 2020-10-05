035d246067a1683a865d9d083351ea318c054f44
nadav <tashernadav@gmail.com>
2020-10-05 12:43:03 +0300

    Dialog dismiss changes

--- template/frontend/modules/external/dialog.js
+++ template/frontend/modules/external/dialog.js
@@ -16,7 +16,7 @@ class Dialog {
                 await Module.import("UI");
 
                 // Parse HTML code
-                let html = atob("PGRpdiBuYW1lPSJvdXRlciIgc3R5bGU9InBvc2l0aW9uOiBmaXhlZDsgbGVmdDogMDsgcmlnaHQ6IDA7IGJvdHRvbTogMDsgd2lkdGg6IDEwMCU7IiByb3cgc3RyZXRjaD4KICAgIDxkaXYgbmFtZT0iaW5uZXIiIGNvbHVtbj4KICAgICAgICA8ZGl2IG5hbWU9ImRpYWxvZyIgc3R5bGU9ImJhY2tncm91bmQtY29sb3I6IHZhcigtLWNvYXN0ZXIpOyIgcm93PgogICAgICAgICAgICA8IS0tIE1lc3NhZ2UgLS0+CiAgICAgICAgICAgIDxwIHNtYWxsIGxlZnQgbmFtZT0ibWVzc2FnZSIgc3R5bGU9IndpZHRoOiAxMDAlOyI+JHttZXNzYWdlfTwvcD4KICAgICAgICAgICAgPCEtLSBCdXR0b24gLS0+CiAgICAgICAgICAgIDxwIHRpbnkgY2VudGVyIG5hbWU9ImNsb3NlIj5EaXNtaXNzPC9wPgogICAgICAgIDwvZGl2PgogICAgPC9kaXY+CjwvZGl2Pg==");
+                let html = atob("PGRpdiBuYW1lPSJvdXRlciIgc3R5bGU9InBvc2l0aW9uOiBmaXhlZDsgbGVmdDogMDsgcmlnaHQ6IDA7IGJvdHRvbTogMDsgd2lkdGg6IDEwMCU7IiByb3cgc3RyZXRjaD4KICAgIDxkaXYgbmFtZT0iaW5uZXIiIGNvbHVtbj4KICAgICAgICA8ZGl2IG5hbWU9ImRpYWxvZyIgc3R5bGU9ImJhY2tncm91bmQtY29sb3I6IHZhcigtLWNvYXN0ZXIpOyIgcm93PgogICAgICAgICAgICA8IS0tIE1lc3NhZ2UgLS0+CiAgICAgICAgICAgIDxwIHNtYWxsIGxlZnQgbmFtZT0ibWVzc2FnZSIgc3R5bGU9IndpZHRoOiAxMDAlOyI+JHttZXNzYWdlfTwvcD4KICAgICAgICAgICAgPCEtLSBCdXR0b24gLS0+CiAgICAgICAgICAgIDxwIHRpbnkgY2VudGVyIHBvaW50ZXIgbmFtZT0iY2xvc2UiPkRpc21pc3M8L3A+CiAgICAgICAgPC9kaXY+CiAgICA8L2Rpdj4KPC9kaXY+");
 
                 // Create a dialog instance
                 let instance = UI.populate(html, {
