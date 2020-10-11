9d5a67ae81b667f8c02cc215b4ee489d1cfdac7a
nadav <tashernadav@gmail.com>
2020-10-11 12:18:03 +0300

    New bundles directory for quick application creation

---
+++ bundles/complete/sources/backend/application.mjs
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2020 Nadav Tasher
+ * https://github.com/NadavTasher/Template/
+ **/
+
+// Import internal parts
+import { Server } from "./internal/server/server.mjs";
+
+// Create the server
+let mServer = new Server(8000);
+
+// Import the routes
+import base from "./external/base.mjs";
+
+// Enable the routes
+mServer.insert("base", base);
+
+// Listen for requests
+mServer.listen();
