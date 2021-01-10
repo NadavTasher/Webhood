5f633aa8339cee0de3d8120b0fdc67343b9a4967
nadav <tashernadav@gmail.com>
2021-01-10 17:22:50 +0200

    Added REPL to help with debugging

---
+++ template/backend/console.mjs
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2021 Nadav Tasher
+ * https://github.com/NadavTasher/Template/
+ **/
+
+// Import readline
+import REPL from "repl";
+
+// Import utilities
+import { Utilities, Validator, Hash, Authority, Database } from "./internal/utilities.mjs";
+
+// Initialize REPL
+let mREPL = REPL.start();
+
+// Update context
+mREPL.context.Utilities = Utilities;
+mREPL.context.Validator = Validator;
+mREPL.context.Hash = Hash;
+mREPL.context.Authority = Authority;
+mREPL.context.Database = Database;
