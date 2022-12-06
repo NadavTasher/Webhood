ab74e11c5e6580e03b18a898825f30f49f6854fe
Nadav Tasher <tashernadav@gmail.com>
2022-12-06 20:13:45 +0000

    Fixes to frontend

--- template/sources/frontend/template/template.js
+++ template/sources/frontend/template/template.js
@@ -1,71 +1,36 @@
 class Module {
     static load() {
-        // Return a multi-promise
         return Promise.all(
-            // For each argument (module name) in the arguments array (arguments passed to function)
-            Array.from(arguments).map(Module.script)
+            Array.from(arguments).map(Module.module)
         );
     }
 
-	static locate(module) {
-        // Initialize name & default sources
-        let name = module.toLowerCase();
-        let repository = "internal";
+	static module(module) {
+		// Transform module name to lowercase
+		module = module.toLowerCase();
 
-        // Slice name and look for repository
-        const slices = name.split(":");
-
-        // Make sure there are exactly two slices
-        if (slices.length === 2) {
-            name = slices.pop();
-            repository = slices.pop();
-        }
-
-        // Query repository element
-        const element = document.querySelector(`meta[name="repository-${repository}"]`);
-
-        // Make sure repository exists
-        if (element !== null)
-            return `${element.content}/${name}`;
-
-        // Return null
-        return null;
-    }
-
-	static script(module) {
 		// Create import promise
 		return new Promise((resolve, reject) => {
-			// Transform module name to lowercase
-			module = module.toLowerCase();
-
-			// Create module element name
-			const id = `module:${module}`;
+			// Create a script element
+			const element = document.createElement("script");
+			element.id = `module:${module}`;
+			element.src = `modules/${module}.js`;
 
 			// Check if already exists
-			if (document.getElementById(id)) {
+			if (document.getElementById(element.id)) {
 				resolve(`Module ${module} was already loaded`);
 				return;
 			}
-
-			// Make sure module is located
-			if (Module.locate(module) === null) {
-				reject(`Module "${module}" was not located`);
-				return;
-			}
-
-			// Create a script tag
-			const element = document.createElement("script");
-
-			// Prepare the script tag
-			element.id = id;
-			element.src = `${Module.locate(module)}.js`;
 
 			// Add success and failure listeners
 			element.addEventListener("load", () => {
 				resolve(`Module "${module}" was loaded`);
 			});
 			element.addEventListener("error", () => {
+				// Failed loading - remove element
 				document.head.removeChild(element);
+
+				// Reject the promise
 				reject(`Module "${module}" was not loaded`);
 			});
 
@@ -75,36 +40,34 @@ class Module {
 	}
 
 	static resources(module) {
-		return new Promise((resolve, reject) => {
-			// Transform module name to lowercase
-			module = module.toLowerCase();
+		// Transform module name to lowercase
+		module = module.toLowerCase();
 
-			// Create module element name
-			const id = `module-html:${module}`;
+		// Create import promise
+		return new Promise((resolve, reject) => {
+			// Create the resources element
+			const element = document.createElement("div");
+			element.id = `resources:${module}`;;
 
 			// Check if already exists
-			if (document.getElementById(id)) {
+			if (document.getElementById(element.id)) {
 				resolve(`Resources for ${module} are already loaded`);
 				return;
 			}
 
 			// Try fetching the HTML resource
-			fetch(`${Module.locate(module)}.html`).then(
+			fetch(`modules/${module}.html`).then(
 				(response) => {
 					response.text().then(
-						(html) => {
+						(resources) => {
 							// Check if already exists
-							if (document.getElementById(id)) {
+							if (document.getElementById(element.id)) {
 								resolve(`Resources for ${module} were loaded already`);
 								return;
 							}
 
-							// Append the HTML to the head
-							document.head.innerHTML += html;
-
-							// Create the element marker
-							const element = document.createElement("meta");
-							element.id = id;
+							// Set the content of the element
+							element.innerHTML = resources;
 
 							// Add the element to the head
 							document.head.appendChild(element);
@@ -122,7 +85,7 @@ class Module {
 // Lock the viewport height to prevent keyboard resizes
 window.addEventListener("load", function () {
     // Query viewport element
-    let element = document.querySelector(`meta[name="viewport"]`);
+    const element = document.querySelector(`meta[name="viewport"]`);
     
     // Make sure viewport exists
     if (element !== null)
