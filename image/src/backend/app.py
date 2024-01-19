e91b8a10d39dbb0e4a3df9de1e69ce126f755565
Nadav Tasher <tashernadav@gmail.com>
2024-01-19 16:00:20 +0200

    Redesign - Use Flask, Gunicorn, NGINX and Supervisor (#3)

---
+++ image/src/backend/app.py
@@ -0,0 +1,17 @@
+import logging
+
+# Import utilities
+from fsdicts import *
+from runtypes import *
+from guardify import *
+
+# Import internal router
+from router import router
+
+# Setup the logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+
+
+@router.post("/api/ping")
+def _ping():
+    return "Pong"
---
+++ image/src/backend/router.py
@@ -0,0 +1,82 @@
+import os
+import traceback
+
+# Import flask utilities
+from flask import Flask, request, jsonify
+from flask.sansio.scaffold import setupmethod
+
+# Initialize debug mode
+DEBUG = int(os.environ.get("DEBUG", "0"))
+
+# Type checking prefix
+PREFIX = "type_"
+
+
+class Router(Flask):
+
+    @setupmethod
+    def route(self, rule, **options):
+
+        # Fetch all the types
+        types = {
+            # Create a key: value without prefix
+            key[len(PREFIX):]: options.pop(key)
+            # For all keys in options
+            for key in list(options)
+            # That start with the prefix
+            if key.startswith(PREFIX)
+        }
+
+        # Create wrapper for route that will get all arguments from request
+        def decorator(function):
+            # Create the wrapper function
+            def wrapper(**kwargs):
+                try:
+                    # Update the kwargs with JSON parameters
+                    if request.is_json:
+                        # Update with JSON content
+                        kwargs.update(request.json)
+
+                    # Update the kwargs with form parameters
+                    if request.form:
+                        kwargs.update(request.form)
+
+                    # Update the kwargs with URL parameters
+                    if request.args:
+                        kwargs.update(request.args)
+
+                    # Validate all the parameters
+                    for key, value_type in types.items():
+                        # Convert the value using the type
+                        kwargs[key] = value_type(kwargs.get(key))
+
+                    # Get the result
+                    result = function(**kwargs)
+
+                    # Check if the result is a response
+                    if isinstance(result, self.response_class):
+                        return result
+
+                    # Create JSON response from result
+                    return jsonify(result), 200
+                except BaseException as exception:
+                    # If debug mode is enabled, return stack
+                    if DEBUG:
+                        return traceback.format_exc(), 500
+
+                    # Create error response from exception
+                    return str(exception), 500
+
+            # Register the wrapper function
+            endpoint = options.pop("endpoint", None)
+            self.add_url_rule(rule, endpoint, wrapper, **options)
+
+            # Return the original function
+            return function
+
+        # Return the decorator
+        return decorator
+
+
+# Initialize the application
+router = Router(__name__)
