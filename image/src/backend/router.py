c32a14dff4317123c333adadb67242ed27337915
Nadav Tasher <tashernadav@gmail.com>
2024-02-06 22:00:29 +0200

    Moved implementation of wrapper to add_url_rule

--- image/src/backend/router.py
+++ image/src/backend/router.py
@@ -5,8 +5,8 @@ import traceback
 # Import flask utilities
 from flask import Flask, request, jsonify
 
-# Initialize debug mode
-DEBUG = int(os.environ.get("DEBUG", "0"))
+# Get debug state
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 # Type checking prefix
 PREFIX_REQUIRED = "type_"
@@ -15,7 +15,7 @@ PREFIX_OPTIONAL = "optional_"
 
 class Router(Flask):
 
-    def route(self, rule, **options):
+    def add_url_rule(self, rule, endpoint=None, view_func=None, provide_automatic_options=None, **options):
         # Fetch all of the required types
         required_types = {
             # Create a key: value without prefix
@@ -36,77 +36,67 @@ class Router(Flask):
             if key.startswith(PREFIX_OPTIONAL)
         }
 
-        # Create wrapper for route that will get all arguments from request
-        def decorator(function):
-            # Create the wrapper function
-            def wrapper(**flask_kwargs):
-                try:
-                    # Update the kwargs with JSON parameters
-                    if request.is_json:
-                        flask_kwargs.update(request.json)
+        # Create wrapper function
+        def view_func_wrapper(**flask_kwargs):
+            try:
+                # Update the kwargs with JSON parameters
+                if request.is_json:
+                    flask_kwargs.update(request.json)
 
-                    # Update the kwargs with form parameters
-                    if request.form:
-                        flask_kwargs.update(request.form)
+                # Update the kwargs with form parameters
+                if request.form:
+                    flask_kwargs.update(request.form)
 
-                    # Update the kwargs with URL parameters
-                    if request.args:
-                        flask_kwargs.update(request.args)
+                # Update the kwargs with URL parameters
+                if request.args:
+                    flask_kwargs.update(request.args)
 
-                    # Create the actual parameters
-                    function_kwargs = dict()
+                # Create the actual parameters
+                function_kwargs = dict()
 
-                    # Validate must-have arguments
-                    for key, value_type in required_types.items():
-                        # Make sure the required argument exists
-                        if key not in flask_kwargs:
-                            raise KeyError("Argument %r is missing" % key)
+                # Validate must-have arguments
+                for key, value_type in required_types.items():
+                    # Make sure the required argument exists
+                    if key not in flask_kwargs:
+                        raise KeyError("Argument %r is missing" % key)
 
-                        # Try casting into the value type
-                        function_kwargs[key] = value_type(flask_kwargs[key])
+                    # Try casting into the value type
+                    function_kwargs[key] = value_type(flask_kwargs[key])
 
-                    # Validate optional arguments
-                    for key, value_type in optional_types.items():
-                        # Check whether the argument exists
-                        if key not in flask_kwargs:
-                            continue
+                # Validate optional arguments
+                for key, value_type in optional_types.items():
+                    # Check whether the argument exists
+                    if key not in flask_kwargs:
+                        continue
 
-                        # Try casting into the value type
-                        function_kwargs[key] = value_type(flask_kwargs[key])
+                    # Try casting into the value type
+                    function_kwargs[key] = value_type(flask_kwargs[key])
 
-                    # Get the result
-                    result = function(**function_kwargs)
+                # Get the result
+                result = view_func(**function_kwargs)
 
-                    # Check if the result is a response
-                    if isinstance(result, self.response_class):
-                        return result
+                # Check if the result is a response
+                if isinstance(result, self.response_class):
+                    return result
 
-                    # Create JSON response from result
-                    return jsonify(result), 200
-                except BaseException as exception:
-                    # If debug mode is enabled, return stack
-                    if DEBUG:
-                        # Log the exception
-                        logging.exception("Exception in %s:", rule)
+                # Create JSON response from result
+                return jsonify(result), 200
+            except BaseException as exception:
+                # If debug mode is enabled, return stack
+                if self.debug:
+                    # Log the exception
+                    logging.exception("Exception in %s:", rule)
 
-                        # Return the full traceback
-                        return traceback.format_exc(), 500
+                    # Return the full traceback
+                    return traceback.format_exc(), 500
 
-                    # Create error response from exception
-                    return str(exception), 500
+                # Create error response from exception
+                return str(exception), 500
 
-            # Decide the endpoint name
-            endpoint = options.pop("endpoint", rule + repr(options.get("methods")))
-
-            # Register the wrapper function
-            self.add_url_rule(rule, endpoint, wrapper, **options)
-
-            # Return the original function
-            return function
-
-        # Return the decorator
-        return decorator
+        # Add the url rule using the parent
+        return super(Router, self).add_url_rule(rule, endpoint, view_func_wrapper, provide_automatic_options, **options)
 
 
 # Initialize the application
 router = Router(__name__)
+router.debug = DEBUG
