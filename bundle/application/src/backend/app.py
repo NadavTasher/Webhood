2b0a849f62c1d1751fd52156f02dd9d5d575d7a0
Nadav Tasher <tashernadav@gmail.com>
2024-03-10 20:25:18 +0200

    Revert "Update bundle"
    This reverts commit 359d001997e91bdaf3b021c5676e1104fb96ba64.

--- bundle/application/src/backend/app.py
+++ bundle/application/src/backend/app.py
@@ -6,24 +6,12 @@ from runtypes import *
 from guardify import *
 
 # Import internal router
-from router import router, sockets
+from router import router
 
 # Setup the logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 
 @router.route("/api/ping", methods=["GET", "POST"], optional_echo=Text)
-def ping_request(echo="Ping"):
-    # Send back the echo
+def ping(echo="Ping"):
     return "Pong %s" % echo
-
-
-@sockets.route("/socket/ping")
-def pong_socket(socket):
-    # Loop until the socket is closed
-    while socket.connected:
-        # Receive the client text
-        ping = socket.receive(10) or "Ping"
-
-        # Send back the name
-        socket.send("Pong %r" % ping)
