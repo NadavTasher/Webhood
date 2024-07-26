6e871c9877d4c1a2fd15778e18d9a8251de6a2eb
Nadav Tasher <tashernadav@gmail.com>
2024-07-26 17:42:12 +0300

    Updated the bundles

--- bundles/independent/application/src/backend/app.py
+++ bundles/independent/application/src/backend/app.py
@@ -3,54 +3,38 @@ from runtypes import *
 from guardify import *
 
 # Import the router
-from utilities import PlainTextResponse, router, redict, relist
+from utilities.redis import broadcast, listen, redict
+from utilities.starlette import router
 
 # Initialize ping database
-DATABASE = redict("ping")
+DATABASE = redict("click")
 DATABASE.setdefaults(count=0)
 
 
-@router.get("/api/code", optional_head=int)
-def code_request(head=None) -> PlainTextResponse:
-    # Read the uptime
-    with open(__file__, "r") as code_file:
-        code = code_file.read()
-
-    # If all data is to be returned, return all
-    if head is None:
-        return PlainTextResponse(code)
-
-    # Split code to lines
-    lines = code.splitlines(keepends=True)
-
-    # Take only specific lines
-    assert 0 < head <= len(lines), "Head range invalid"
-
-    # Take just the first "head" lines
-    return PlainTextResponse(str().join(lines[:head]))
-
-
-@router.post("/api/ping", optional_echo=Text, optional_content_type=Text, optional_content_data=Bytes)
-def ping_request(echo=None, content_type=None, content_data=None) -> str:
+@router.post("/api/click")
+def click_request() -> str:
     # Increment ping count
     DATABASE.count += 1
 
     # Return the ping count
-    return "Ping %r (count=%d)" % (echo or content_data, DATABASE.count)
+    return "Click count is %d" % DATABASE.count
 
 
-@router.socket("/socket/ping", optional_initial=Text)
-async def ping_socket(websocket, initial="Ping") -> None:
-    # Send the initial string
-    await websocket.send_text(initial)
+@router.post("/api/relay", type_message=Text)
+async def relay_request(message):
+    # Publish to channel
+    await broadcast(text=message)
 
-    # Loop until client closes
-    while websocket:
-        # Receive the next string from the client
-        next_string = await websocket.receive_text()
 
-        # Send the same string
-        await websocket.send_text(next_string)
+@router.socket("/socket/relay")
+async def relay_socket(websocket) -> None:
+    # Accept the websocket
+    await websocket.accept()
+
+    # Subscribe to the global relay
+    async for event in listen():
+        # Send the message
+        await websocket.send_text(event.text)
 
 
 # Initialize the application
